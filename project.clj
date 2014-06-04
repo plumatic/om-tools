72ae1ac5286dfca1bf15e66cf0df970b0f67328a
loganlinn <logan.linn@gmail.com>
2014-06-04 12:21:37 -0700

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/om-tools "0.1.0-SNAPSHOT"
+(defproject prismatic/om-tools "0.1.0"
   :description "Tools for building expressive Om applications"
   :url "http://github.com/prismatic/om-tools"
   :license {:name "Eclipse Public License - v 1.0"
