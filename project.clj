dd138c59aa8650c7a4f673f5ae378c3e37fb10df
loganlinn <logan.linn@gmail.com>
2014-10-10 23:44:15 -0700

    lein-release plugin: bumped version from 0.3.4 to 0.3.5-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject prismatic/om-tools "0.3.4"
+(defproject prismatic/om-tools "0.3.5-SNAPSHOT"
   :description "Tools for building Om applications"
   :url "http://github.com/prismatic/om-tools"
   :license {:name "Eclipse Public License - v 1.0"
